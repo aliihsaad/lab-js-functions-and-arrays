5437baeeff0e77dd754994133c0365cece52942d
Ali Saad <59366979+aliihsaad@users.noreply.github.com>
2026-03-19 19:47:48 +0100

    solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if(num1 > num2) {
+        return num1
+    } else {
+        return num2;
+    }
+}
 
 
 
@@ -7,7 +13,19 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if ( words.length === 0) {
+        return null;
+    }
+
+let longest = words[0];
+for (let i = 0; i < words.length; i++) {
+    if (longest.length < words[i].length) {
+        longest = words[i];
+    }
+}
+return longest;
+}
 
 
 
@@ -15,7 +33,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i];
+    }
+    return total;
+}
 
 
 
@@ -23,7 +47,15 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0;
+    }
+    let total = sumNumbers(numbers2);
+    let Average = total / numbers2.length;
+    
+    return Average
+}
 
 
 
@@ -31,4 +63,21 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, word) {
+  if (arrayOfWords.length === 0) {
+    return null;
+  } else if (arrayOfWords.includes(word)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function doesWordExist(arrayOfWords, word) {
+//     if (arrayOfWords.length === 0) {
+//         return null;
+//     }
+//     return arrayOfWords.includes(word);
+
+    
+// }
